efcd4fcaf75590666194969fd0c1bbdadd0fb833
taiga-gw <t.shiokawa@gurukun-web.com>
2025-04-01 10:10:09 +0900

    add map section

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import styles from './Map.module.css'
+import Image from 'next/image'
 export default function Map() {
   return (
     <div className={styles.container}>
       {/* 背景画像 */}
       <div className={styles.bg}>
-        {/* 半透明のカバー画像 */}
-        <div className={styles.cover}></div>
-        {/* コンテンツ */}
-        <div className={styles.textArea}>
-          {/* 会社ロゴ */}
+        <div className={styles.content}>
+          {/* 会社ロゴ & 住所 */}
+          <div className={styles.logo}>
+            <Image src="/images/site-logo-map.png" width={561} height={86} alt="logo" />
+            <p className={styles.address}>所在地／〒158-0083 東京都世田谷区奥沢5-42-3 tefu 2F D-11</p>
+          </div>
+          {/* コンテンツ */}
+          <div className={styles.map}>
+            <Image src="/images/map.png" width={1714} height={685} alt="logo" />
+          </div>
+          <div className={styles.businessLogo}>
+            <Image src="/images/business-logo-ps.png" width={1193} height={249} alt="logo"></Image>
+          </div>
         </div>
       </div>
     </div>
