04dabf81bdef511e5e4b75adaca1e95c257100fe
taiga-gw <t.shiokawa@gurukun-web.com>
2025-05-07 10:03:29 +0900

    add form

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -15,18 +15,20 @@ export default function Contact() {
           以下よりお問い合わせください。</p>
         </div>
         {/* お問い合わせボタン */}
-        <div className={styles.contactButtonArea}>
-          <div className={styles.contactButtonInner}>
-            <Image src="/images/contact-icon-pc.png" width={56} height={40} alt='contact-button' className={styles.contactButtonIcon} />
-            <button 
-              type='submit' 
-              className={styles.contactButton}
-            >
-              ご相談はこちら
-            </button>
-            <Image src="/images/arrow-icon-pc.png" width={63} height={16} alt='contact-button' className={styles.contactButtonIcon} />
+        <a href="https://www.w-pro.jp/form.html">
+          <div className={styles.contactButtonArea}>
+            <div className={styles.contactButtonInner}>
+              <Image src="/images/contact-icon-pc.png" width={56} height={40} alt='contact-button' className={styles.contactButtonIcon} />
+              <button 
+                type='submit' 
+                className={styles.contactButton}
+              >
+                ご相談はこちら
+              </button>
+              <Image src="/images/arrow-icon-pc.png" width={63} height={16} alt='contact-button' className={styles.contactButtonIcon} />
+            </div>
           </div>
-        </div>
+        </a>
       </div>
     </div>
   )
